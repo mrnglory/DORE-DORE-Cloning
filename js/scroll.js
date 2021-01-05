cba479ed6db1ba0723d56291cf21eec2431094ee
Joeun <mrnglory@mail.hongik.ac.kr>
2021-01-06 00:33:25 +0900

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+		$(document).ready(function(){
+			$('#fullpage').fullpage({
+				scrollingSpeed:500,
+				loopBottom:true
+			});
+		});
---
+++ js/submenuToggle.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+    $(document).ready(function(){
+        $(".menu-icon").click(function(){
+            $(".submenu2").toggle();
+        });
+    });
